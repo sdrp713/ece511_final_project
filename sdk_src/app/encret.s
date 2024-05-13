e5e3276510980fa7ba03b2c0e0858a7685384a64
sdrp713 <100436830+sdrp713@users.noreply.github.com>
2024-05-12 22:35:53 -0500

    Delete sdk_src directory

--- sdk_src/app/encret.s
+++
@@ -1,5 +0,0 @@
-.globl EAPP_RETURN
-EAPP_RETURN:
-	li a7, 1101
-	ecall
-	
--- sdk_src/verifier/keys.cpp
+++
@@ -1,7 +0,0 @@
-//******************************************************************************
-// Copyright (c) 2018, The Regents of the University of California (Regents).
-// All Rights Reserved. See LICENSE for license details.
-//------------------------------------------------------------------------------
-#include <Keys.hpp>
-
-PublicKey::PublicKey(std::string hexstr) {}
